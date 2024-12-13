5b340a0f0bc3082fa554b24bbf91af2332ae17dc
Andre Wagner <andre.nho@gmail.com>
2024-12-13 09:52:23 -0600

    .

--- backup.sh
+++ backup.sh
@@ -4,6 +4,9 @@ BUCKET=s3://andrenho-retropie-backup
 
 # calculate uncompressed size, ask for confirmation
 du -h --max-depth=0 ~/RetroPie /opt/retropie/configs ~/.aws
+echo "Total files:"
+find ~/RetroPie -type f | wc -l
+find /opt/retropie/configs -type f | wc -l
 
 read -p "Continue backup? (Y/N) " -n 1 -r
 echo
@@ -11,8 +14,8 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit; fi
 
 # upload all files
 
-aws s3 sync /home/pi/.aws $BUCKET/ --storage-class STANDARD_IA
-aws s3 sync /opt/retropie/configs $BUCKET/ --storage-class STANDARD_IA
-aws s3 sync /home/pi/RetroPie $BUCKET/ --storage-class STANDARD_IA
+aws s3 sync /home/pi/.aws $BUCKET/.aws --storage-class ONEZONE_IA
+aws s3 sync /opt/retropie/configs $BUCKET/opt_retropie_configs --storage-class ONEZONE_IA
+aws s3 sync /home/pi/RetroPie $BUCKET/RetroPie --storage-class ONEZONE_IA
 
 echo "Done."
